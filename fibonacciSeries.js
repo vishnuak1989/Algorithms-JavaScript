758d02a2fd8294431e129bd41ae5b1fb094072a2
Vishnu Anilkumar <vishnuak1989@gmail.com>
2018-04-23 16:18:38 -0400

    Create fibonacciSeries.js

---
+++ fibonacciSeries.js
@@ -0,0 +1,14 @@
+//Fibonnaci Series of n length
+
+const fibonacci=(n)=> {
+  let fibSeries=[0 , 1]
+  for(i = 2; i<n; i++)
+    {
+      let num = fibSeries[i-1]+ fibSeries[i-2]
+      fibSeries.push(num)
+    }
+}
+
+//Test Case
+fibonacci(10)
+console.log(fibSeries)
